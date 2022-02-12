e17bce3b31d947e9ee97faa6a50f5bfd4c5b501a
dcortez4 <dcortez4@csub.edu>
2022-02-12 15:37:20 -0800

    Started A2

---
+++ MultiThreadSort/src/a2/dcortez/Main.java
@@ -0,0 +1,12 @@
+package a2.dcortez;
+
+import a1.dcortez.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Food f = a1.dcortez.Main.genFood();
+        System.out.println(f);
+    }
+}
--- inventory/src/a1/dcortez/Main.java
+++ inventory/src/a1/dcortez/Main.java
@@ -6,69 +6,77 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    private static final Random ran = new Random();
+    private static final FoodItems[] foodItems = FoodItems.values();
+    private static final Tools[] tools = Tools.values();
+    private static final ToolUses[] toolUses = ToolUses.values();
+    private static final Booster[] boosterItems = Booster.values();
+    private static final Clothes[] clothes = Clothes.values();
+    private static final ClothingType [] clothingTypes = ClothingType.values();
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Random ran = new Random();
         List<Item> items = new ArrayList<>();
-        FoodItems[] foodItems = FoodItems.values();
-        Tools[] tools = Tools.values();
-        ToolUses[] toolUses = ToolUses.values();
-        Booster[] boosterItems = Booster.values();
-        Clothes[] clothes = Clothes.values();
-        ClothingType [] clothingTypes = ClothingType.values();
-
-
+        Scanner scan = new Scanner(System.in);
+        // Start of the app
         System.out.print("How many items do you want: ");
         int itemCnt = Integer.parseInt(scan.nextLine());
 
-        for(int i=0; i<itemCnt; i++){
+        for (int i = 0; i < itemCnt; i++) {
             int type = ran.nextInt(4);
             switch (type) {
-                case 0 -> {
-                    int foodIndex = ran.nextInt(foodItems.length);
-                    String foodName = foodItems[foodIndex].toString();
-                    float foodPrice = ran.nextFloat(10);
-                    int foodQty = ran.nextInt(30);
-                    int foodUses = ran.nextInt(6);
-                    float healthGain = ran.nextFloat(20);
-                    Food tempFood = new Food(foodName, foodPrice, foodQty, foodUses, healthGain);
-                    items.add(tempFood);
-                }
-                case 1 -> {
-                    int toolIndex = ran.nextInt(tools.length);
-                    String toolName = tools[toolIndex].toString();
-                    float toolPrice = ran.nextFloat(200);
-                    int toolQty = ran.nextInt(15);
-                    String use = toolUses[toolIndex].toString();
-                    Tool tempTool = new Tool(toolName, toolPrice, toolQty, use);
-                    items.add(tempTool);
-                }
-                case 2 -> {
-                    int boostersIndex = ran.nextInt(boosterItems.length);
-                    String boosterName = boosterItems[boostersIndex].toString();
-                    float boosterPrice= ran.nextFloat(1000);
-                    int boosterQty = ran.nextInt(20);
-                    int boosterUses = ran.nextInt(10);
-                    float bonus = ran.nextFloat(100);
-                    Boosters tempBooster = new Boosters(boosterName, boosterPrice, boosterQty, boosterUses, bonus);
-                    items.add(tempBooster);
-                }
-                case 3 -> {
-                    int clothingIndex = ran.nextInt(clothes.length);
-                    String clothingName = clothes[clothingIndex].toString();
-                    float clothingPrice = ran.nextFloat(500);
-                    int clothingQty = ran.nextInt(10);
-                    String cType = clothingTypes[clothingIndex].toString();
-                    Clothing tempClothing = new Clothing(clothingName,clothingPrice,clothingQty, cType);
-                    items.add(tempClothing);
-                }
+                case 0 -> items.add(genFood());
+
+                case 1 -> items.add(genTool());
+
+                case 2 -> items.add(genBooster());
+
+                case 3 -> items.add(genClothing());
+
             }
         }
 
-        for(Item i : items) {
-            System.out.println(i);
+        for (Item i : items) {
+                System.out.println(i);
         }
 
     }
+
+    public static Food genFood(){
+        int foodIndex = ran.nextInt(foodItems.length);
+        String foodName = foodItems[foodIndex].toString();
+        float foodPrice = ran.nextFloat(10);
+        int foodQty = ran.nextInt(30);
+        int foodUses = ran.nextInt(6);
+        float healthGain = ran.nextFloat(20);
+        return new Food(foodName, foodPrice, foodQty, foodUses, healthGain);
+    }
+
+    public static Tool genTool(){
+        int toolIndex = ran.nextInt(tools.length);
+        String toolName = tools[toolIndex].toString();
+        float toolPrice = ran.nextFloat(200);
+        int toolQty = ran.nextInt(15);
+        String use = toolUses[toolIndex].toString();
+        return new Tool(toolName, toolPrice, toolQty, use);
+    }
+
+    public static Boosters genBooster(){
+        int boostersIndex = ran.nextInt(boosterItems.length);
+        String boosterName = boosterItems[boostersIndex].toString();
+        float boosterPrice = ran.nextFloat(1000);
+        int boosterQty = ran.nextInt(20);
+        int boosterUses = ran.nextInt(10);
+        float bonus = ran.nextFloat(100);
+        return new Boosters(boosterName, boosterPrice, boosterQty, boosterUses, bonus);
+    }
+
+    public static Clothing genClothing(){
+        int clothingIndex = ran.nextInt(clothes.length);
+        String clothingName = clothes[clothingIndex].toString();
+        float clothingPrice = ran.nextFloat(500);
+        int clothingQty = ran.nextInt(10);
+        String cType = clothingTypes[clothingIndex].toString();
+        return new Clothing(clothingName, clothingPrice, clothingQty, cType);
+    }
+
 }
